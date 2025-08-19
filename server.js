10130e24b0db77e01663b55cbed9da85d8d8099e
Tunde Abdulhamid Sanusi <52820977+tuhamworld@users.noreply.github.com>
2025-08-19 07:39:44 +0100

    chore: Initial fileread code

--- server.js
+++ server.js
@@ -10,11 +10,19 @@ const __dirname = import.meta.dirname;
 const server = http.createServer((req, res) => {
 
   const pathToResource = path.join(__dirname, "public", "index.html");
-  const content = fs.readFileSync(pathToResource, "utf-8");
-  
+  // const content = fs.readFileSync(pathToResource, "utf-8");
+
+  fs.readFile(pathToResource, "utf-8", (err, content) => {
+    if (err) {
+      console.log(err)
+      return
+    }
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
   res.end(content);
+  })
+  
+
 })
 
 server.listen(PORT, () => console.log('connected on port 8000'))
