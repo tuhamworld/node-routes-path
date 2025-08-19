b8531ae6e800fd3d636e1592088f975035b13914
Tunde Abdulhamid Sanusi <52820977+tuhamworld@users.noreply.github.com>
2025-08-19 10:56:32 +0100

    chore: serving index.html without encoding

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 import http from "node:http";
 import path from "node:path";
-import fs from "node:fs"
+import fs from "node:fs/promises"
 import { testPath } from './utils/testPath.js';
 
 const PORT = 8000;
 
 const __dirname = import.meta.dirname;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
 
   const pathToResource = path.join(__dirname, "public", "index.html");
-  // const content = fs.readFileSync(pathToResource, "utf-8");
+  const content = await fs.readFile(pathToResource);
 
-  fs.readFile(pathToResource, "utf-8", (err, content) => {
-    if (err) {
-      console.log(err)
-      return
-    }
+
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
   res.end(content);
-  })
+
   
 
 })
