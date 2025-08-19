34a00bd41448ae2274dca064f40c470d7133547b
Tunde Abdulhamid Sanusi <52820977+tuhamworld@users.noreply.github.com>
2025-08-19 07:25:43 +0100

    feat: read html content in NodeJS

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
-import http from 'node:http';
-import path from 'node:path';
+import http from "node:http";
+import path from "node:path";
+import fs from "node:fs"
 import { testPath } from './utils/testPath.js';
 
 const PORT = 8000;
 
 const __dirname = import.meta.dirname;
 
-// console.log("CWD: ", process.cwd());
-// console.log("Directory Name: ", import.meta.dirname);
-
 const server = http.createServer((req, res) => {
 
-  const absPathToResource = path.join(__dirname, "public", "index.html");
-  const relPathToResource = path.join("public", "index.html");
-  console.log("absolute path: ", absPathToResource);
-  console.log("relative path:", relPathToResource);
-  testPath(__dirname);
+  const pathToResource = path.join(__dirname, "public", "index.html");
+  const content = fs.readFileSync(pathToResource, "utf-8");
   
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end()
+  res.end(content);
 })
 
 server.listen(PORT, () => console.log('connected on port 8000'))
--- utils/testPath.js
+++ utils/testPath.js
@@ -1,8 +1,6 @@
 import path from "node:path";
 
 export const testPath = (dir) => {
-    // const relPathToResource = path.join("public", "index.html");
     const absPathToResource = path.join(dir, "public", "index.html");
-    // console.log("test path: ", relPathToResource);
     console.log("test absolute path: ", absPathToResource);
 }
